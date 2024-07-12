73ebab9ba4f3b70a16dc43fd12bdcb3e28f25137
bethoc <bethoconnelldev@gmail.com>
2024-07-12 18:58:48 +0100

    add employee table with database data, header and footer

---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const FooterComponent = () => {
+  return (
+    <footer className="footer p-5 text-center mt-auto">
+      <p>
+        <span>Check out my GitHub!  </span>
+        <button type="button" className="btn btn-outline-light btn-rounded">
+          Go
+        </button>
+      </p>
+    </footer>
+  );
+};
+
+export default FooterComponent;
